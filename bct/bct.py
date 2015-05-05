Bug fix for charpath which accounts for 1) NaN when calculating min/max for eccentricity, as well as 2) changing diagonals from 0's to inf to not include them in the calculation of lambda, which introduced error with factor 1/N (see original BCT function)

bct/bct.py
@@ -1606,19 +1606,19 @@ the main diagonal.
     D = D.copy()
 
     if not include_diagonal:
-        np.fill_diagonal(D, 0)
+        np.fill_diagonal(D, np.inf)
 
     #mean of finite entries of D[G]
     lambda_=np.sum(D[D!=np.inf])/len(np.where(D!=np.inf)[0])	
 
     #eccentricity for each vertex (ignore inf)
-    ecc=np.max(D*(D!=np.inf),axis=1)
+    ecc=np.nanmax(D*(D!=np.inf),axis=1)
 
     #radius of graph
-    radius=np.min(ecc)	#but what about zeros?
+    radius=np.nanmin(ecc)	#but what about zeros?
 
     #diameter of graph
-    diameter=np.max(ecc)
+    diameter=np.nanmax(ecc)
 
     #efficiency: mean of inverse entries of D[G]
     n=len(D)